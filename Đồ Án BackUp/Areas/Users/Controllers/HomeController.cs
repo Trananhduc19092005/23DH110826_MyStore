1378b7ea07c93282b1f38dd957335dd20c09a8f5
Trananhduc19092005 <duc19092005f@gmail.com>
2024-10-17 20:16:40 +0700

    Update Add Catogeries Items

---
+++ Đồ Án BackUp/Areas/Users/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Đồ_Án_BackUp.Areas.Users.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Users/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
